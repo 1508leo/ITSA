555f79f38d368dac6602dfc6768a2ef04d366114
Leo Hsu <leo@LeodeMacBook-Pro.local>
2023-10-19 16:43:50 +0800

    10/19

---
+++ One_on_One.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Qsort */
+int compare(const void *a, const void *b){
+    return *(int *)a - *(int *)b;
+}
+
+int main(){
+    int amount, win=0, s1=0, s2=0;
+    int *enemy, *player;
+
+    while(1){
+        scanf(" %d", &amount);
+        if(amount == 0)
+            break;
+
+        // Reset
+        win = 0;
+        s1 = 0;
+        s2 = 0;
+
+        enemy = malloc(amount * sizeof(int));
+        player = malloc(amount * sizeof(int));
+
+        for(int i = 0; i < amount; i++)
+            scanf(" %d", &enemy[i]);
+        
+        for(int i = 0; i < amount; i++)
+            scanf(" %d", &player[i]);
+
+        qsort(enemy, amount, sizeof(int), compare);
+        qsort(player, amount, sizeof(int), compare);
+
+        // Compare
+        while(s2 < amount){
+            if(enemy[s1] < player[s2]){
+                win++;
+                s1++;
+                s2++;
+            }
+            else{
+                s2++;
+            }
+        }
+
+        free(enemy);
+        free(player);
+
+        printf("%d\n", win);
+    }
+
+    return 0;
+}
---
+++ Rank_Student_Grade.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+
+// Store information of students
+struct student{
+    int number;
+    char name[100];
+    int grade;
+    int rank;
+};
+
+struct student class[1000];
+
+int main(){
+    int number, grade, rank, amount=0;
+    char name[100];
+    int ranking[2][100] = {0};
+
+    // Only 6 students
+    for(int i = 0; i < 6; i++){
+        scanf(" %d %s %d", &number, name, &grade);
+
+        // Store data
+        class[amount].number = number;
+        strcpy(class[amount].name, name);
+        class[amount].grade = grade;
+
+        ranking[0][amount] = grade;
+        amount++;
+    }
+
+    // Rank
+    for(int i = 0; i < amount; i++){
+        ranking[1][i]++;
+        for(int j = 0; j < amount; j++){
+            if(ranking[0][i] < ranking[0][j])
+                ranking[1][i]++;
+        }
+
+        class[i].rank = ranking[1][i];
+    }
+
+    // Print
+    for(int i = 0; i < amount; i++){
+        printf("%d %s %d %d\n", class[i].number, class[i].name, class[i].grade, class[i].rank);
+    }
+
+    return 0;
+}
